91101f8ea3aafffbbc47cfb6369fd472ad7eec08
VadymPopov <boycristal10@gmail.com>
2023-12-23 14:53:14 -0500

    add: logic, css, packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { Component } from "react";
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { defaultMarkdown } from "./defaultMarkdown";
+import './App.css';
 
-const App = () => {
-  return (
-    <>
-       <h1>React Template</h1>
-    </>
-  )
-};
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { 
+      input: defaultMarkdown,
+    }
+    this.onInput = this.onInput.bind(this);
+  }
+  
+  onInput (e) {
+    this.setState({
+      input: e.target.value,
+    })
+  }
+  
+  render() {
+    return (
+      <div className='app'>
+        <textarea autoFocus id="editor" value={this.state.input} onChange={this.onInput}/>
+        <div id="preview" className='markdown'>
+        <Markdown  remarkPlugins={[remarkGfm]}>{this.state.input}</Markdown>
+          </div>
+        </div>
+    );
+  }
+}
+
 
 export default App;
---
+++ src/defaultMarkdown.js
@@ -0,0 +1,30 @@
+export const defaultMarkdown = `# Welcome to Markdown Previewer 
+
+## This is your canvas. Just create
+
+### Guide and syntax
+- [markdown guide](https://www.markdownguide.org/)
+
+- [markdown syntax](https://daringfireball.net/projects/markdown/syntax)
+
+## Inline code
+This website is using \`react-markdown\`
+
+## Blocks of code
+
+\`\`\`
+let message = 'Hello world';
+alert(message);
+\`\`\`
+
+
+
+## Blockquotes
+>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
+For example [GitHub](https://github.com/VadymPopov) README.md
+
+![react logo](https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg)
+
+
+Coded by **Vadym Popov**
+`;
